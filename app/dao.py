21431b0c71ef5ac76cdd09a559a79361e8c38f3c
Phuc092001 <phuc670481@gmail.com>
2023-11-09 10:09:52 +0700

    demo CNPM

---
+++ app/dao.py
@@ -0,0 +1,18 @@
+from app.models import Category, Product, User
+
+
+def load_categories():
+    return Category.query.all()
+
+
+def load_products(kw=None):
+    products = Product.query
+
+    if kw:
+        products = products.filter(Product.name.contains(kw))
+
+    return products.all()
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
---
+++ app/models.py
@@ -0,0 +1,61 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    avatar = Column(String(100), default='https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    products = relationship('Product', backref='category', lazy=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    price = Column(Float, default=0)
+    image = Column(String(100))
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+if __name__ == "__main__":
+    from app import app
+    with app.app_context():
+        db.create_all()
+
+        c1 = Category(name='Mobile')
+        c2 = Category(name='Tablet')
+
+        db.session.add(c1)
+        db.session.add(c2)
+        db.session.commit()
+
+        p1 = Product(name='iPad Pro 2022', price=24000000, category_id=2,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p2 = Product(name='iPhone 13', price=21000000, category_id=1,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p3 = Product(name='Galaxy S23', price=24000000, category_id=1,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p4 = Product(name='Note 22', price=22000000, category_id=1,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p5 = Product(name='Galaxy Tab S9', price=24000000, category_id=2,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+
+        db.session.add_all([p1, p2, p3, p4, p5])
+        db.session.commit()
