c185c6a22ef2dfd8ca7da96f775169d9863ad460
Phuc092001 <phuc670481@gmail.com>
2023-11-15 09:28:49 +0700

    Update

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,22 @@ from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin, BaseView, expose
 from app import app, db
 from app.models import Category, Product
+from flask_login import logout_user, current_user
+from flask import redirect
+from app.models import UserRoleEnum
 
 
 admin = Admin(app=app, name='QUẢN TRỊ BÁN HÀNG', template_mode='bootstrap4')
+
+
+class AuthenticatedUser(BaseView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class AuthenticatedAdmin(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.user_role == UserRoleEnum.ADMIN
 
 
 class MyProductView(ModelView):
@@ -25,6 +38,14 @@ class MyStatsView(BaseView):
         return self.render('admin/stats.html')
 
 
+class MyLogoutView(AuthenticatedUser):
+    @expose("/")
+    def index(self):
+        logout_user()
+        return redirect('/admin')
+
+
 admin.add_view(MyCategoryView(Category, db.session))
 admin.add_view(MyProductView(Product, db.session))
 admin.add_view(MyStatsView(name='Thống kê báo cáo'))
+admin.add_view(MyLogoutView(name='Đăng xuất'))
--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from app import db
 from flask_login import UserMixin
+import enum
+
+
+class UserRoleEnum(enum.Enum):
+    USER = 1
+    ADMIN = 2
 
 
 class User(db.Model, UserMixin):
@@ -9,7 +15,9 @@ class User(db.Model, UserMixin):
     name = Column(String(50), nullable=False)
     username = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
-    avatar = Column(String(100), default='https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg')
+    avatar = Column(String(100),
+                    default='https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg')
+    user_role = Column(Enum(UserRoleEnum), default=UserRoleEnum.USER)
 
     def __str__(self):
         return self.name
@@ -33,11 +41,22 @@ class Product(db.Model):
     image = Column(String(100))
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
 
+    def __str__(self):
+        return self.name
+
 
 if __name__ == "__main__":
     from app import app
     with app.app_context():
         db.create_all()
+
+        import hashlib
+        u = User(name='Admin',
+                 username='admin',
+                 password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
+                 user_role=UserRoleEnum.ADMIN)
+        db.session.add(u)
+        db.session.commit()
 
         c1 = Category(name='Mobile')
         c2 = Category(name='Tablet')
@@ -56,6 +75,17 @@ if __name__ == "__main__":
                      image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
         p5 = Product(name='Galaxy Tab S9', price=24000000, category_id=2,
                      image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p6 = Product(name='iPad Pro 2023', price=24000000, category_id=2,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p7 = Product(name='iPhone 15 Pro', price=21000000, category_id=1,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p8 = Product(name='Galaxy S24', price=24000000, category_id=1,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p9 = Product(name='Note 23 Pro', price=22000000, category_id=1,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
+        p10 = Product(name='Galaxy Tab S9 Ultra', price=24000000, category_id=2,
+                     image="https://res.cloudinary.com/dp87nci6w/image/upload/v1698970995/tablet.jpg")
 
         db.session.add_all([p1, p2, p3, p4, p5])
+        db.session.add_all([p6, p7, p8, p9, p10])
         db.session.commit()
